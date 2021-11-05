f40b5c49d857676b738d105f7819cd751573dada
ShoaibSikander <shoaib.pu@gmail.com>
2021-11-05 22:54:53 +0100

    Uploaded project as an initial commit

---
+++ Client/Client/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+
+namespace Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Client started !");
+
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var customerClient = new Customer.CustomerClient(channel);
+            var clientRequested = new CustomerLookupModel { UserId = 2 };
+            var reply = await customerClient.GetCustomerInfoAsync(clientRequested);
+            Console.WriteLine($"{reply.FirstName} {reply.LastName}");
+        }
+    }
+}
---
+++ Server/Server/Services/CustomerService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+
+namespace Server.Services
+{
+    public class CustomerService : Customer.CustomerBase
+    {
+        private readonly ILogger<CustomerService> _logger;
+        public CustomerService(ILogger<CustomerService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<CustomerModel> GetCustomerInfo(CustomerLookupModel request, ServerCallContext context)
+        {
+            //return base.GetCustomerInfo(request, context);
+            CustomerModel output = new CustomerModel();
+            if (request.UserId == 1)
+            {
+                output.FirstName = "Shoaib";
+                output.LastName = "Sikander";
+            }
+            else if (request.UserId == 2)
+            {
+                output.FirstName = "Muhammad";
+                output.LastName = "Shoaib";
+            }
+            else            {
+                output.FirstName = "Muhammad";
+                output.LastName = "Sikander";
+            }
+            return Task.FromResult(output);
+        }
+        
+    }
+}
